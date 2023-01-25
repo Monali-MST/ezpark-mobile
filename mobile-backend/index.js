14e83aa4319af3543133c4a246c3fb6ad7b5cb07
Pramod Jay <91390000+pramod-jay@users.noreply.github.com>
2023-01-25 12:17:15 +0530

    Backend and frontend

--- mobile-backend/index.js
+++ mobile-backend/index.js
@@ -3,10 +3,12 @@ import mysql from "mysql";
 
 const app = express()
 
+app.use(express.json())
+
 const db=mysql.createConnection({
     host:"ezpark-db.cvhbqqtsx1je.ap-northeast-1.rds.amazonaws.com",
     user:"admin",
-    passwrod:"ezPark!123",
+    password:"ezPark!123",
     database:"EzPark"
 
 })
@@ -21,6 +23,23 @@ app.get("/user",(req,res)=>{
         if(err) return res.json(err)
         return res.json(data)
     })
+})
+
+app.post("/user",(req,res)=>{
+    const query="INSERT INTO `EzPark`.`User_Details` (`UserID`, `Firstname`, `Lastname`, `Email`, `UserPassword`, `Contact`) VALUES (?);"
+    const values=[
+        req.body.UserID,
+        req.body.Firstname,
+        req.body.Lastname,
+        req.body.Email,
+        req.body.UserPassword,
+        req.body.Contact,
+    ]
+    db.query(query,[values],(err,data)=>{
+        if(err) return res.json(err)
+        return res.json("User account has been  created succefully")
+    })
+
 })
 
 app.listen(8800, ()=>{
